79d109d6299bcb81945d89474127abdbf9d0480e
mgomezu <49196702+mgomezu@users.noreply.github.com>
2021-11-08 00:10:00 -0500

    Add files via upload

--- Parcial1/Punto4Client.py
+++ Parcial1/Punto4Client.py
@@ -1,17 +1,30 @@
 import socket
 import asyncio
+import os
 
 host = socket.gethostname()
 port = 12345
 BUFFER_SIZE = 1024
-FILE = "ejemploPunto4.txt"
-
+archivo = "ejemploPunto4.txt"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
     socket_tcp.connect((host, port))
-    socket_tcp.send(FILE.encode())
-    data = socket_tcp.recv(BUFFER_SIZE)
-    f = open (data.decode('utf-8'),'r')
-    mensaje = f.read()
-    print(mensaje)
+    while True:
+        f = open(archivo, "rb")
+        content = archivo.encode() + f.read(BUFFER_SIZE)
+        while content:
+            socket_tcp.send(content)
+            content = f.read(BUFFER_SIZE)
+        break
+    try:
+        socket_tcp.send(chr(1))
+
+    except TypeError:
+        socket_tcp.send(bytes(chr(1), "utf-8"))
+        
     f.close()
+    print("El archivo ha sido enviado correctamente.")
+    data = socket_tcp.recv(BUFFER_SIZE)
+    print(data.decode())
+    socket_tcp.close()
+
--- Parcial1/Punto4Server.py
+++ Parcial1/Punto4Server.py
@@ -5,26 +5,36 @@ import shutil
 host = socket.gethostname() 
 port = 12345
 BUFFER_SIZE = 1024
-lista_archivos = "lista_archivos.txt"
-f = open('lista_archivos.txt','a')
+
+lista_archivos = os.getcwd() + '\ArchivosPunto4'
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
     socket_tcp.bind((host, port)) 
-    socket_tcp.listen(1) 
+    socket_tcp.listen(5) 
     conn, addr = socket_tcp.accept() 
-    with conn:
-        print("Conexion establecida") 
-        data = conn.recv(BUFFER_SIZE)
-        if not data:
-            f.close()
-            conn.send("error al enviar archivo".encode())
-            conn.close()
+    with conn:        
+        try:
+            input_data = conn.recv(BUFFER_SIZE)
+        except error:
+            print("Error de lectura.")
+            socket_tcp.close()
         else:
-            ruta = os.getcwd() + os.sep
-            origen = ruta + data.decode('utf-8')
-            destino = ruta + 'ArchivosPunto4//' + data.decode('utf-8')
-            shutil.copy(origen, destino)
-            f.write(data.decode('utf-8') + '\n')  
-        f.close()
-        conn.send(lista_archivos.encode())
-        conn.close()
+            if input_data:
+                if isinstance(input_data, bytes):
+                    end = input_data[0] == 1
+                else:
+                    end = input_data == chr(1)
+                if not end:
+                    input_data = input_data.decode()
+                    input_data = input_data.split(sep='.txt', maxsplit=2)
+                    f = open("ArchivosPunto4/"+input_data[0]+'.txt', "w")
+                    f.write(input_data[1])
+                else:
+                    print("El archivo se ha recibido correctamente.")
+                    contenido = os.listdir(lista_archivos)
+                    conn.send(contenido.encode())
+                    f.close()
+        print("El archivo se ha recibido correctamente.")
+        contenido = os.listdir(lista_archivos)
+        conn.send(str(contenido).encode('utf-8'))
